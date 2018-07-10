79afbd8fa05ae0a1b532422fe219f324d6d89597
LuanC03 <luan.bezerra@ccc.ufcg.edu.br>
2018-07-10 00:43:59 -0300

    Enviando CalculaTempoVoo

--- src/pratica3_p2/Voo.java
+++ src/pratica3_p2/Voo.java
@@ -1,6 +1,5 @@
 package pratica3_p2;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Voo {
 
@@ -8,7 +7,7 @@ public class Voo {
 	private String destino;
 	private String partidaPrevista;
 	private String chegadaPrevista;
-	private double tempoDeVoo;
+	private String tempoDeVoo;
 	
 	public Voo(String origem, String destino, String partidaPrevista, String chegadaPrevista) throws ParseException {
 		this.origem = origem;
@@ -18,21 +17,39 @@ public class Voo {
 		this.tempoDeVoo = calculaTempoVoo(partidaPrevista, chegadaPrevista);
 	}
 
-	public double calculaTempoVoo(String partidaPrevista, String chegadaPrevista) throws ParseException {
-		SimpleDateFormat sdFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-        java.util.Date dataInicio = sdFormat.parse(partidaPrevista);
-        java.util.Date dataFim = sdFormat.parse(chegadaPrevista);
-        double diff = dataFim.getTime() - dataInicio.getTime();
-        double resultado = diff / (1000*60*60);
-		return resultado;
+	public String calculaTempoVoo(String partidaPrevista, String chegadaPrevista) throws ParseException {
+//		SimpleDateFormat sdFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+//        java.util.Date dataInicio = sdFormat.parse(partidaPrevista);
+//        java.util.Date dataFim = sdFormat.parse(chegadaPrevista);
+//        double diff = dataFim.getTime() - dataInicio.getTime();
+//        double resultado = diff / (1000*60*60);
+		String[] partida = partidaPrevista.split(" ");
+		String[] chegada = chegadaPrevista.split(" ");
+		String retorno = calculaHora(partida[1], chegada[1]); 
+		return retorno;
+	}
+
+	private String calculaHora(String partida, String chegada) {
+		String[] string = partida.split(":");
+		int horaPartida = Integer.parseInt(string[0]);
+		int minutoPartida = Integer.parseInt(string[1]);
+		String[] string2 = chegada.split(":");
+		int horaChegada = Integer.parseInt(string2[0]);
+		int minutoChegada = Integer.parseInt(string2[1]);
+		String retorno ="";
+		if(horaChegada>=horaPartida)
+			retorno = (horaChegada-horaPartida) +":" +(((minutoChegada+60)-minutoPartida)%60);
+		else{
+			if(minutoPartida == 0) 
+				retorno  = ((24-horaPartida)+horaChegada) + ":" + (minutoChegada);
+			else
+				retorno = ((24-horaPartida)+horaChegada)-1 +":" +(((minutoChegada+60)-minutoPartida)%60);
+		}
+		return retorno;
 	}
 
 	public String toString() {
-		return origem + " " + destino + " " + partidaPrevista + " " + chegadaPrevista + " Tempo de Voo: " + format(tempoDeVoo);
-	}
-	
-	private String format(double x) {  
-	    return String.format("%.1f", x);  
+		return origem + " " + destino + " " + partidaPrevista + " " + chegadaPrevista + " Tempo de Voo: " + tempoDeVoo;
 	}
 	
 	public String getOrigem() {
@@ -51,7 +68,7 @@ public class Voo {
 		return chegadaPrevista;
 	}
 
-	public double getTempoDeVoo() {
+	public String getTempoDeVoo() {
 		return tempoDeVoo;
 	}
 }
