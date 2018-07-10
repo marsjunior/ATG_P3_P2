a8bd5fc17e9368a2c35a9e4a89ca1769750f7320
LuanC03 <luan.bezerra@ccc.ufcg.edu.br>
2018-07-10 11:48:53 -0300

    Metodo de interacao Melhor Voo

--- src/pratica3_p2/Main.java
+++ src/pratica3_p2/Main.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
 public class Main {
 	private static ArrayList<Voo> melhoresVoos = new ArrayList<>();
 	private static ArrayList<EmpresaAerea> empresas = new ArrayList<>();
-	private static String[] aeroportosBrInter = { "SBJP", "SBGR", "SBBR", "SBGL", "SBSV", "SBCF", "SBFZ", "SBEG", "SBPA",
-			"SBRF", "SBCT", "SBBE", "SBKP", "SBSG", "SBFL", "SBBV", "SBMQ", "SBPV", "SBSN", "SBCY", "SBCG", "SBMO",
-			"SBFI", "SBCB", "SBCZ", "SDSC", "SBRB", "SBTT", "SBPB" };
-	
-	private static String[] aeroportosBr = {"SBSP", "SBRJ", "SBVT", "SBGO", "SBPS", "SBAR", "SBTE", "SBUL", "SBRP",
-			"SBLO", "SBBH", "SBRB", "SBJU", "SBMG", "SBMO", "SSPS", "SBJV", "SBCX", "SBPL", "SBKG", "SBSN", "SBIL", "SBIZ",
-			"SBIZ", "SBCH", "SBMA", "SBDN", "SBDN", "SJTC", "SBMK"};
+	private static String[] aeroportosBrInter = { "SBJP", "SBGR", "SBBR", "SBGL", "SBSV", "SBCF", "SBFZ", "SBEG",
+			"SBPA", "SBRF", "SBCT", "SBBE", "SBKP", "SBSG", "SBFL", "SBBV", "SBMQ", "SBPV", "SBSN", "SBCY", "SBCG",
+			"SBMO", "SBFI", "SBCB", "SBCZ", "SDSC", "SBRB", "SBTT", "SBPB" };
+
+	private static String[] aeroportosBr = { "SBSP", "SBRJ", "SBVT", "SBGO", "SBPS", "SBAR", "SBTE", "SBUL", "SBRP",
+			"SBLO", "SBBH", "SBRB", "SBJU", "SBMG", "SBMO", "SSPS", "SBJV", "SBCX", "SBPL", "SBKG", "SBSN", "SBIL",
+			"SBIZ", "SBIZ", "SBCH", "SBMA", "SBDN", "SBDN", "SJTC", "SBMK" };
 
 	private static String[] aeroportosEua = { "KJFK", "KLGA", "KEWR", "KBOS", "KORD", "KIAD", "KDCA", "KATL", "KMCO",
 			"KMIA", "KIAH", "KDFW", "KLAX", "KLAS", "PHNL", "KSFO", "KSEA", "KDEN", "KDTW", "KLAN", "KPHX", "KMDW",
-			"KSLC", "SBJP", "SBGR", "SBBR", "SBGL", "SBSV", "SBCF", "SBFZ", "SBEG", "SBPA",
-			"SBRF", "SBCT", "SBBE", "SBKP", "SBSG", "SBFL", "SBBV", "SBMQ", "SBPV", "SBSN", "SBCY", "SBCG", "SBMO",
-			"SBFI", "SBCB", "SBCZ", "SDSC", "SBRB", "SBTT", "SBPB"};
+			"KSLC", "SBJP", "SBGR", "SBBR", "SBGL", "SBSV", "SBCF", "SBFZ", "SBEG", "SBPA", "SBRF", "SBCT", "SBBE",
+			"SBKP", "SBSG", "SBFL", "SBBV", "SBMQ", "SBPV", "SBSN", "SBCY", "SBCG", "SBMO", "SBFI", "SBCB", "SBCZ",
+			"SDSC", "SBRB", "SBTT", "SBPB" };
 	private static Scanner scanner;
-	
+
 	private static boolean containsArray(String[] array, String aeroporto) {
 		for (int i = 0; i < array.length; i++) {
 			if (array[i].equals(aeroporto)) {
@@ -40,26 +40,27 @@ public class Main {
 		while (scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
 			String[] voo = linha.split(";");
-			if (containsArray(aeroportosBrInter, voo[4]) && (containsArray(aeroportosBr, voo[5]) || containsArray(aeroportosEua, voo[5]))) {
+			if (containsArray(aeroportosBrInter, voo[4])
+					&& (containsArray(aeroportosBr, voo[5]) || containsArray(aeroportosEua, voo[5]))) {
 				Voo vooDaVez = null;
 				if (!voo[6].equals("") && !voo[8].equals("")) {
 					vooDaVez = new Voo(voo[4], voo[5], voo[6], voo[8]);
-					if(verificaVoo(vooDaVez))
+					if (verificaVoo(vooDaVez))
 						melhoresVoos.add(vooDaVez);
-				}else if (!voo[6].equals("") && !voo[9].equals("")) {
+				} else if (!voo[6].equals("") && !voo[9].equals("")) {
 					vooDaVez = new Voo(voo[4], voo[5], voo[6], voo[9]);
-					if(verificaVoo(vooDaVez))
+					if (verificaVoo(vooDaVez))
 						melhoresVoos.add(vooDaVez);
-				}else if (!voo[7].equals("") && !voo[8].equals("")) {
+				} else if (!voo[7].equals("") && !voo[8].equals("")) {
 					vooDaVez = new Voo(voo[4], voo[5], voo[7], voo[8]);
-					if(verificaVoo(vooDaVez))
+					if (verificaVoo(vooDaVez))
 						melhoresVoos.add(vooDaVez);
-				}else if (!voo[7].equals("") && !voo[9].equals("")) {
+				} else if (!voo[7].equals("") && !voo[9].equals("")) {
 					vooDaVez = new Voo(voo[4], voo[5], voo[7], voo[9]);
-					if(verificaVoo(vooDaVez))
+					if (verificaVoo(vooDaVez))
 						melhoresVoos.add(vooDaVez);
 				}
-				
+
 			}
 		}
 	}
@@ -67,26 +68,43 @@ public class Main {
 	private static boolean verificaVoo(Voo vooDaVez) {
 		boolean retorno = true;
 		for (Voo voo : melhoresVoos) {
-			if(voo.getOrigem().equals(vooDaVez.getOrigem())
-					&&voo.getDestino().equals(vooDaVez.getDestino())
-					&&!voo.getTempoDeVooString().equals(vooDaVez.getTempoDeVooString())) {
-				voo = melhorVoo(voo, vooDaVez);
+			if (voo.getOrigem().equals(vooDaVez.getOrigem()) && voo.getDestino().equals(vooDaVez.getDestino())
+					&& !voo.getTempoDeVooString().equals(vooDaVez.getTempoDeVooString())) {
+				voo = comparaVoos(voo, vooDaVez);
 				retorno = false;
-			}else if(voo.getOrigem().equals(vooDaVez.getOrigem())
-					&&voo.getDestino().equals(vooDaVez.getDestino())
-					&&voo.getTempoDeVooString().equals(vooDaVez.getTempoDeVooString()))
+			} else if (voo.getOrigem().equals(vooDaVez.getOrigem()) && voo.getDestino().equals(vooDaVez.getDestino())
+					&& voo.getTempoDeVooString().equals(vooDaVez.getTempoDeVooString()))
 				retorno = false;
-		}	
+		}
 		return retorno;
 	}
 
-	private static Voo melhorVoo(Voo voo1, Voo voo2) {
-		Voo retorno = null;		
-		if(voo1.getTempoDeVooMinutos()>voo2.getTempoDeVooMinutos())
+	private static Voo comparaVoos(Voo voo1, Voo voo2) {
+		Voo retorno = null;
+		if (voo1.getTempoDeVooMinutos() > voo2.getTempoDeVooMinutos())
 			retorno = voo2;
 		else
 			retorno = voo1;
 		return retorno;
+	}
+
+	private static void melhorVoo() {
+		Scanner sc = new Scanner(System.in);
+		String continuar = "sim";
+		while (continuar.equalsIgnoreCase("sim")) {
+			System.out.printf("Origem: ");
+			String origem = sc.nextLine();
+			System.out.printf("Destino: ");
+			String destino = sc.nextLine();			
+			for (Voo voo : melhoresVoos) {
+				if (voo.getOrigem().equalsIgnoreCase(origem) && voo.getDestino().equalsIgnoreCase(destino))
+					System.out.println("Aeroporto de Origem (Sigla): " + origem + ", Aeroporto de Destino (Sigla): " + destino
+							+ ", Tempo Medio de Duracao do Voo: " + voo.getTempoDeVooString());
+			}
+			System.out.printf("Deseja continuar a pesquisa? (Sim) ou (Nao): ");
+			continuar = sc.nextLine();
+		}
+		sc.close();
 	}
 
 	public static void readEmpresas() throws FileNotFoundException {
@@ -107,10 +125,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		readVoos();
 		readEmpresas();
+		scanner.close();
 		for (Voo voo : melhoresVoos) {
 			System.out.println(voo.toString());
 		}
-		System.out.println("Voos: " + melhoresVoos.size() + " Aeroportos Br: " + aeroportosBr.length + " Aeroportos Eua: "
-				+ aeroportosEua.length);
+		melhorVoo();
+		System.out.println("Voos: " + melhoresVoos.size() + " Aeroportos Br: " + aeroportosBr.length
+				+ " Aeroportos Eua: " + aeroportosEua.length);		
 	}
 }
